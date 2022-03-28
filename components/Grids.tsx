e9df916a683dd7e07db60cf6ecfc1738633d2ddb
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2022-03-28 16:35:29 +0900

    [new] 프로젝트 시작, header / footer 추가
    next.js 사용한 path-finder 프로젝트 시작
    - 환경 구성

---
+++ components/Grids.tsx
@@ -0,0 +1,5 @@
+const Grids = () => {
+  return <div></div>;
+};
+
+export default Grids;
---
+++ components/HeadTag.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const HeadTag = () => {
+  return (
+    <Head>
+      <title>Path Finder</title>
+      <meta name="description" content="Path Finder App" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default HeadTag;
