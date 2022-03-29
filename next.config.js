9c8632d802474b5866744c98e01517fe8f8d75e5
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2022-03-29 16:12:43 +0900

    [update] next.config.js 내부 경로 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const debug = process.env.NODE_ENV !== 'production';
+const name = 'world-calendar';
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: !debug ? `/${name}/` : '',
+};
